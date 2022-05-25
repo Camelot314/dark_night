908851f8fa1e392700eb4ef2c60361c5b4c7acb3
Jaraad Kamal <kjaraad@gmail.com>
2022-05-25 16:11:34 -0400

    added functionality to change the background color of the main tag and the tag with the id 'content'

--- night.js
+++ night.js
@@ -1,6 +1,90 @@
 "use strict";
 // strict typing
 
+// Function that gets the main tag if there is one on the page
+// otherwise it returns null.
+function getMainTag() {
+    let mainTags = document.getElementsByTagName("main");
+    let hasMain = mainTags.length > 0;
+
+    return hasMain ? mainTags[0] : null
+}
+
+// function that gets the element with the content id.
+// It is to keep calls consistent. 
+function getContentTag() {
+    return document.getElementById("content")
+}
+
+/* 
+    Function that will get the three main colors in the web page. 
+    This is the color of the body tag. The color of the main tag (if present).
+    And the color of the tag with id "content" (if present).
+    It will return an object with the three colors saved.
+    It one of the above tags are missing then that respective color will be
+    set to null. 
+*/
+function readInitial() {
+    let bodyColor = window.getComputedStyle(document.body).backgroundColor;
+    let mainColor = null;
+    let contentColor = null;
+
+    let mainTag = getMainTag();
+    if (mainTag != null) {
+        mainColor = window.getComputedStyle(mainTag).backgroundColor;
+        // console.log("mainColor: ", mainColor);
+    }
+
+    let content = getContentTag();
+    if (content != null) {
+        contentColor = window.getComputedStyle(content).backgroundColor;
+        // console.log("content color: ", contentColor);
+    }
+    // console.log({b: bodyColor, m: mainColor, c: contentColor});
+    return {b: bodyColor, m: mainColor, c: contentColor}
+}
+
+/* 
+    Changing the background color of the three important tags. 
+    The body tag. The main tag and the tag with an id of "content".
+    The latter two may not be present so it is handled with if statments. 
+*/
+function nightMode() {
+    console.log("turning to night");
+    let mainTag = getMainTag();
+    let contentTag = getContentTag();
+
+    document.body.style.backgroundColor = 'black';
+
+    if (mainTag != null) {
+        mainTag.style.backgroundColor = 'black';
+        console.log("changing color of mainTag");
+    }
+
+    if (contentTag != null) {
+        contentTag.style.backgroundColor = 'black';
+        console.log("changing color of contentTag");
+    }
+}
+
+/* 
+    Function that will reset the background color of the three 
+    important tags.
+*/
+function resetColors(colors) {
+    console.log("resetting");
+    document.body.style.backgroundColor = colors.b;
+
+    if (colors.m != null) {
+        let mainTag = getMainTag();
+        mainTag.style.backgroundColor = colors.m;
+    }
+
+    if (colors.c != null) {
+        let contentTag = getContentTag();
+        contentTag.style.backgroundColor = colors.c;
+    }
+}
 
 function main() {
     // intial console output
@@ -9,10 +93,9 @@ function main() {
     // variable to tell if the content should be dark mode or not
     let active = false;
 
-    // getting the original color
-    let originalColor = window.getComputedStyle(document.body).backgroundColor;
-    console.log("color: ", originalColor);
-
+    // getting the original colors
+    let originalColors = readInitial();
+    console.log("colors: ", originalColors);
 
     /* 
         This function wil enforce the dark mode on the webpage. 
@@ -35,11 +118,9 @@ function main() {
     function night() {
         active = !active;
         if (active) {
-            console.log("turning to night");
-            document.body.style.backgroundColor = 'black';
+            nightMode();
         } else {
-            console.log("resetting");
-            document.body.style.backgroundColor = originalColor;
+            resetColors(originalColors);
         }
 
         return active;
@@ -65,8 +146,8 @@ function main() {
         enabled on the current page. 
     */
     browser.runtime.onMessage.addListener(request => {
-        console.log("Message from the background script:");
-        console.log(request.greeting);
+        // console.log("Message from the background script:");
+        // console.log(request.greeting);
         let is_active = night();
 
         return Promise.resolve({response: is_active});
