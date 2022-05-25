6ed5753219e58904841fe97ac14523d72e6328e0
Jaraad Kamal <kjaraad@gmail.com>
2022-05-24 21:48:26 -0400

    adding comments to the functions

--- background.js
+++ background.js
@@ -1,12 +1,58 @@
-function openPage(tab) {
-    // browser.tabs.create({
-    //   url: "https://developer.mozilla.org"
-    // });
-    console.log("background");
-    console.log(tab);
-  }
+"use strict";
+
+// function that handles errors (just prints)
+function onError(error) {
+    console.error(`Error: ${error}`);
+}
+
+
+/* 
+    main function that is run every time that the
+    toolbar button is run. It will check all the tabs
+    that are active and in the window. It will then 
+    call the send Messge function 
+*/
+function main() {
+    console.log("button was clicked");
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(sendMessageToTabs).catch(onError);
+}
   
-browser.browserAction.onClicked.addListener(openPage);
+/* 
+    This will send a message to the content script (night.js)
+    so that it can edit the content of the of the individual page
+    This background script does not have access to that content. 
+    the purpose of sending a message is to inform the individual page
+    that the button has been pressed and to update the page to dark mode
+    variant. 
+*/
+function sendMessageToTabs(tabs) {
+    // sending a message to each tab
+    for (let tab of tabs) {
+        // the message greeting will be 'true'
+        browser.tabs.sendMessage(
+            tab.id,
+            {greeting: true}
+        ).then(response => {
+            // expecting a response that from the active tab
+            // prints response
+            console.log("Message from the content script:");
+            console.log(response.response);
+        }).catch(onError);
+        /*         
+        errors are thrown if 
+        1) content script does not send a response back
+        2) button pressed on a page where the content script (night.js)
+             is not allowed to run on (priveledged page) 
+        */
+    }
+}
+
+// adding a listener that will run the main function every time the button toolbar
+// button is pressed. 
+browser.browserAction.onClicked.addListener(main);
 
 
   
--- night.js
+++ night.js
@@ -1,7 +1,63 @@
+"use strict";
+// strict typing
+
+// intial console output
+console.log("initial content");
+
+// global variable to tell if the content should be dark mode or not
+let active = false;
+
+
+/* 
+    This function wil enforce the dark mode on the webpage. 
+    Probably through editing the page.
+
+    It will toggle the active state. It is called every time
+    the background script sends a message (this happens when the
+        tooltip button is pressed)
+    
+    If active then it will turn the page to dark mode. Else reset it.
+    Done imperitively.
+*/
 function night() {
-    console.log("content script");
+    active = !active;
+    if (active) {
+        console.log("turning to night");
+        
+    } else {
+        console.log("resetting");
+    }
+
+    return active;
 }
 
-console.log("initial content")
 
-// browser.browserAction.onClicked.addListener(set);
+
+
+/* 
+    Adding a listener to check for a message from the background script.
+    The only message that the background script should be sending is 
+    an indication of `true` if the button has been pressed.
+
+    When the script is then notified it will complete the code in the brackets.
+    This code will log the message from the background scipt.
+    It will then call the night function to process everything.
+
+    NOTE: It does not check to see what the request greeing is because
+    I am assuming there is only one request that will be sent from the 
+    background script. 
+
+    Finally it will resolve and return the promive with the output of night.
+    The output of night will be an indication of if the forced dark mode is 
+    enabled on the current page. 
+*/
+browser.runtime.onMessage.addListener(request => {
+    console.log("Message from the background script:");
+    console.log(request.greeting);
+
+    let is_active = night();
+
+
+    return Promise.resolve({response: is_active});
+});
+
